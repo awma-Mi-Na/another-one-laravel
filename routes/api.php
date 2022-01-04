cb079e7c15648e3c6ef09fe4df560e6daf39197d
awma-Mi-Na <awmach.9@gmail.com>
2022-01-04 17:04:30 +0530

    maimia

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/login', function () {
+    $attributes = request()->validate([
+        'email' => 'required|email',
+        'password' => 'required'
+    ]);
+    if (Auth::attempt($attributes))
+        return request()->user()->createToken('authToken')->plainTextToken;
+});
